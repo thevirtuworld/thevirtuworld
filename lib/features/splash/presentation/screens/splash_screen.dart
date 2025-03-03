84485ff8d3e028fabbabc03b5ce6731bf8f572bc
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-03-03 23:44:49 +0100

    ✨ M(lib/features/splash/presentation/screens/splash_screen.

--- lib/features/splash/presentation/screens/splash_screen.dart
+++ lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:thevirtuworld/features/auth/presentation/screens/login_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Splash Screen')));
+    return const Scaffold(
+      // body: Center(child: Text('Splash Screen')),
+
+      // add button that leads to login screen
+      body: Center(
+        child: ElevatedButton(
+
+          // when button is pressed go to login
+          onPressed: ,
+           
+          // onHover: ButtonStyle.new(backgroundColor: Color.alphaBlend(foreground, background)),
+
+      
+      )
+
+
+    );
   }
 }
